cb934db71326ca93f8923aa138ba89bee8afbcfe
Tom Fletcher <tom@tomfletcher.tech>
2024-03-26 13:00:12 +0000

    added Codeium

--- init.lua
+++ init.lua
@@ -229,6 +229,7 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
   'fatih/vim-go',
+  'Exafunction/codeium.vim',
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
